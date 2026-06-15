05e22266a1ba122f8e1a059378c06f8714e1c156
Mathias Carolino <mathias@Mathiass-MacBook-Air.local>
2026-06-15 15:32:11 -0300

    feat -> endpoint register feito e testado - criando o SecurityFilterChain com csrf disable para testes

---
+++ src/main/java/br/com/api/auth/controller/AuthController.java
@@ -0,0 +1,32 @@
+package br.com.api.auth.controller;
+
+import br.com.api.auth.dto.RegisterRequestDTO;
+import br.com.api.auth.dto.RegisterResponseDTO;
+import br.com.api.auth.service.AuthService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterResponseDTO> register(
+            @RequestBody @Valid RegisterRequestDTO request
+    ) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(authService.register(request));
+    }
+}
